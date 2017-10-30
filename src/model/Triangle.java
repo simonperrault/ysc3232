19dd1afee105cc42d18428aad474f9fa25beca74
Simon Perrault <perrault.simon@gmail.com>
2017-10-30 15:54:58 +0800

    Changing the implementation of the triangle

--- src/model/Triangle.java
+++ src/model/Triangle.java
@@ -4,19 +4,20 @@ import java.awt.Point;
 
 public class Triangle {
 
-	private Point[] points;
+	private Point p1;
+	private Point p2;
+	private Point p3;
 	
 	public Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
 	{
-		this.points = new Point[3];
-		this.points[0] = new Point(x1, y1);
-		this.points[1] = new Point(x2, y2);
-		this.points[2] = new Point(x3, y3);
+		this.p1 = new Point(x1, y1);
+		this.p2 = new Point(x2, y2);
+		this.p3 = new Point(x3, y3);
 	}
 	
 	public Point getFirstPoint()
 	{
-		return this.points[0];
+		return this.p1;
 	}
 	
 
