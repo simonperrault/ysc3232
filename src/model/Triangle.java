9115bac444c1ce8e3efb0047b21ece09e814852f
Simon Perrault <perrault.simon@gmail.com>
2017-10-30 16:34:25 +0800

    Cleaned the problematic merge. Not good to go!

--- src/model/Triangle.java
+++ src/model/Triangle.java
@@ -17,9 +17,10 @@ public class Triangle {
 	 */
 	public Triangle(int x1, int y1, int x2, int y2, int x3, int y3)
 	{
-		this.p1 = new Point(x1, y1);
-		this.p2 = new Point(x2, y2);
-		this.p3 = new Point(x3, y3);
+		this.points = new Point[3];
+		this.points[0] = new Point(x1, y1);
+		this.points[1] = new Point(x2, y2);
+		this.points[2] = new Point(x3, y3);
 	}
 	/**
 	 * Getter to get the first point of the triangle.
